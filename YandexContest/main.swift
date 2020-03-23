7df42754d96acf6c1b10aed1afcaa0766b3dcb4c
Denis Roenko <denis@roenko.ru>
2020-03-23 17:14:00 +0300

    Initial Commit

---
+++ YandexContest/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  YandexContest
+//
+//  Created by Роенко Денис on 23.03.2020.
+//  Copyright © 2020 Denis Roenko. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
