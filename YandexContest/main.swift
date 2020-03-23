6b75786f0e98659508fcce67a1a250c12ff49806
Denis Roenko <denis@roenko.ru>
2020-03-23 18:22:32 +0300

    initial

--- YandexContest/main.swift
+++ YandexContest/main.swift
@@ -8,5 +8,31 @@
 
 import Foundation
 
-print("Hello, World!")
+var inString = ""
 
+do {
+    inString = try String(contentsOfFile: "input.txt")
+} catch {
+    
+    print("Failed reading from input.txt, Error: " + error.localizedDescription)
+}
+
+let input = inString.dropLast()
+
+let nums = input.split(separator: " ")
+
+var result = 0
+
+nums.forEach {
+    let num = Int($0)!
+    result += num
+}
+
+let outString = String(result)
+
+do {
+    try outString.write(to: URL(fileURLWithPath: "output.txt"), atomically: true, encoding: .utf8)
+} catch {
+    
+    print("Failed writing from output.txt, Error: " + error.localizedDescription)
+}
